a18306c65c09bc717447e54147ab55afb425ad8d
QuocDung21 <rongvang2357hn@gmail.com>
2023-12-06 01:01:38 +0700

    add

--- Controllers/Mess.Controller.js
+++ Controllers/Mess.Controller.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const Home = require('../Models/Home.model');
 const Esp = require("../Models/Esp.model");
+const {home} = require("nodemon/lib/utils");
 
 module.exports = {
     getDenPhongKhach: async (req, res, next) => {
@@ -26,10 +27,28 @@ module.exports = {
                 return res.status(200).json({messages: [{text: messtext}]})
             }
 
-            if (led_1 === 1) {
+            if (parseInt(status) === 1) {
                 let messtext = 'Đã bật đèn phòng khách';
+                await Home.findByIdAndUpdate(
+                    {_id: "656e303ac83ee75dbcc35f26"},
+                    {
+                        led_1: 1
+                    },
+                    {
+                        new: true
+                    }
+                );
                 return res.status(200).json({messages: [{text: messtext}]})
             } else {
+                await Home.findByIdAndUpdate(
+                    {_id: "656e303ac83ee75dbcc35f26"},
+                    {
+                        led_1: 0
+                    },
+                    {
+                        new: true
+                    }
+                );
                 let messtext = 'đèn đã được tắt ạ';
 
                 return res.status(200).json({messages: [{text: messtext}]})
@@ -40,6 +59,60 @@ module.exports = {
                 led_2,
                 door,
             });
+        } catch (error) {
+            console.error(error.message);
+            return res.status(500).json({error: "Internal Server Error"});
+        }
+    },
+
+
+    getDenPhongNgu: async (req, res, next) => {
+        const status = req.params.status;
+        try {
+            const result = await Home.findOne({_id: "656e303ac83ee75dbcc35f26"}, {__v: 0});
+            console.log(result)
+            if (!result) {
+                return res.status(404).json({error: "Data not found"});
+            }
+
+            const led_1 = result.led_1;
+            const led_2 = result.led_2;
+            const door = result.door;
+
+            if (led_2 === parseInt(status)) {
+                let messtext = 'Đèn chưa bật mà sao tắt';
+                if (led_2 === 1) {
+                    messtext = 'Đèn đang bật mà';
+                }
+                return res.status(200).json({messages: [{text: messtext}]})
+            }
+
+            if (parseInt(status) === 1) {
+                let messtext = 'Đã bật đèn phòng khách';
+                await Home.findByIdAndUpdate(
+                    {_id: "656e303ac83ee75dbcc35f26"},
+                    {
+                        led_1: 1
+                    },
+                    {
+                        new: true
+                    }
+                );
+                return res.status(200).json({messages: [{text: messtext}]})
+            } else {
+                await Home.findByIdAndUpdate(
+                    {_id: "656e303ac83ee75dbcc35f26"},
+                    {
+                        led_2: 0
+                    },
+                    {
+                        new: true
+                    }
+                );
+                let messtext = 'đèn đã được tắt ạ';
+
+                return res.status(200).json({messages: [{text: messtext}]})
+            }
         } catch (error) {
             console.error(error.message);
             return res.status(500).json({error: "Internal Server Error"});
--- Routes/Mess.route.js
+++ Routes/Mess.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Mess = require('../Controllers/Mess.Controller');
 
 router.get('/den-phongkhach/:status', Mess.getDenPhongKhach);
-
+router.get('/den-phongngu/:status', Mess.getDenPhongNgu);
 router.post('/', Mess.updateDataHome)
 
 module.exports = router;
