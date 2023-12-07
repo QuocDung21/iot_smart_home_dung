35b2e9d332efe614c68a53b1de6c2fdb6dc25d10
QuocDung21 <rongvang2357hn@gmail.com>
2023-12-07 07:37:49 +0700

    add

--- Controllers/Esp.Controller.js
+++ Controllers/Esp.Controller.js
@@ -14,17 +14,25 @@ module.exports = {
                     "quocdung112001@gmail.com",
                     "Cảnh báo! Nhiệt độ cao",
                     "Cảnh báo! Nhiệt độ cao",
-                    `Nhiệt độ hiện tại là: ${temp} độ C. Vui lòng kiểm tra hệ thống.</p>`
-                )
+                    `
+                        <p style="font-size: 16px; color: #ff0000;">Nhiệt độ hiện tại là: ${temp} độ C. Vui lòng kiểm tra hệ thống.</p>
+                        <img src="https://i.pinimg.com/736x/5e/7b/2a/5e7b2a3f61a11c75edba60d7320e9a79.jpg" alt="Temperature Warning" style="max-width: 100%;">
+                        `
+                );
             }
+
             if (chuyendong == 1) {
                 await sendEmail(
                     "quocdung112001@gmail.com",
                     "Cảnh báo! Phát hiện có người",
                     "Cảnh báo! Phát hiện có người",
-                    `Cảnh báo phát hiện có chuyển động.</p>`
-                )
+                    `
+                        <p style="font-size: 16px; color: #0066cc;">Cảnh báo phát hiện có chuyển động.</p>
+                        <img src="https://i.pinimg.com/736x/5e/7b/2a/5e7b2a3f61a11c75edba60d7320e9a79.jpg" alt="Motion Detection" style="max-width: 100%;">
+                        `
+                );
             }
+
             const espData = new Esp({temp, humid, chuyendong});
             const result = await espData.save();
             res.send(result);
